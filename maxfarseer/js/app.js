26d6757b710afd2b5a3480b229136ae6a8c84549
Roman Alekseev <roman.alekseev.ru@gmail.com>
2017-05-25 23:37:48 +0300

    maxfarseer: Props usage

--- maxfarseer/js/app.js
+++ maxfarseer/js/app.js
@@ -1,3 +1,19 @@
+var my_news = [
+  {
+    author: 'Саша Печкин',
+    text: 'В четверг, четвертого числа...'
+  },
+  {
+    author: 'Просто Вася',
+    text: 'Считаю, что $ должен стоить 35 рублей!'
+  },
+  {
+    author: 'Гость',
+    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+  }
+];
+
+
 var Comments = React.createClass({
   render: function() {
     return (
@@ -8,27 +24,40 @@ var Comments = React.createClass({
   }
 });
 
+
 var News = React.createClass({
   render: function() {
+    var data = this.props.data;
+    var newsTemplate = data.map(function(item, index) {
+      return (
+        <div key={index}>
+          <p className="news__author">{item.author}:</p>
+          <p className="news__text">{item.text}</p>
+        </div>
+      )
+    })
+
     return (
       <div className="news">
-        К сожалению, новостей нет.
+        {newsTemplate}
       </div>
     );
   }
 });
+
 
 var App = React.createClass({
   render: function() {
     return (
       <div className="app">
         Всем привет, я компонент App! Я умею отображать новости.
-        <News />
-      	<Comments />
+        <News data={my_news} /> {/*добавили свойство data */}
+        <Comments />
       </div>
     );
   }
 });
+
 
 ReactDOM.render(
   <App />,
