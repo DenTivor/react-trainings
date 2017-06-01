0f2ea84897ccaf3dc3036be411a23e1cbe2dbc26
Roman Alekseev <roman.alekseev.ru@gmail.com>
2017-06-02 02:33:25 +0300

    Add Isaenok project

---
+++ typeahead-isaenok/client/main.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { Store, createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import App from './main/components/App'
+import rootReducer from './main/reducer'
+
+let store: Store<any> = createStore(rootReducer) //configuration store with empty initial state
+
+console.log(store.getState());
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('app')
+);
